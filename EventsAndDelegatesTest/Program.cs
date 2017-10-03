7368e1753eeab729c8e9978b92fd9f268ecdb2a0
xavier casafont rodriguez <xaviercasafont@hotmail.es>
2017-10-03 22:42:38 +0200

    test

---
+++ EventsAndDelegatesTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegatesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strin232 = "test";
+        }
+    }
+}
