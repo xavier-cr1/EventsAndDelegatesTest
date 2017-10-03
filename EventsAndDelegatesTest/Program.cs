36eea0559bdd05c13c58cc22dea3b180da27096c
xavier casafont rodriguez <xaviercasafont@hotmail.es>
2017-10-04 00:43:35 +0200

    delegates

--- EventsAndDelegatesTest/Program.cs
+++ EventsAndDelegatesTest/Program.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace EventsAndDelegatesTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var strin232 = "test222";
+            var delegate1 = new WorkPerformanceDelegate.WorkPerformanceHandler(WorkPerformance1);
+            delegate1(5, WorkType.HeroesOfTheStorm);
+
+            Console.Read();
+        }
+
+        static void WorkPerformance1(int hours, WorkType workType)
+        {
+            Console.WriteLine($"Performance 1 called: + {hours}");
         }
     }
 }
---
+++ EventsAndDelegatesTest/WorkPerformance/WorkPerformanceDelegate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegatesTest
+{
+    public static class WorkPerformanceDelegate
+    {
+        public delegate void WorkPerformanceHandler(int hours, WorkType workType);
+    }
+}
